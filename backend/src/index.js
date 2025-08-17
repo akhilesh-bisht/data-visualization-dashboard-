a9cf4338b0c8322fbdec56101eb9d470d157b33b
Akhilesh Bisht <akhileshbisht2020@gmail.com>
2025-08-17 23:16:55 +0530

    some pritiier code formating

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,17 +3,16 @@ import { app } from "./app.js";
 const PORT = process.env.PORT || 3000;
 import { connectDB } from "./config/db.js";
 
-
 const startServer = async () => {
-    try{
-        await connectDB();
-        app.listen(PORT, () => {
-            console.log(`Server is running on port ${PORT}`);
-        });
-    }catch (error) {
-        console.error('Error starting the server:', error.message);
-        process.exit(1); 
-    }
-}
+  try {
+    await connectDB();
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Error starting the server:", error.message);
+    process.exit(1);
+  }
+};
 
 startServer();
